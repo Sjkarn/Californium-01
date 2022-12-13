7016eb1a975d3976f649636ecefdbc59df29a023
Saurabh Karn <saurabhkarn007@gmail.com>
2022-12-13 20:15:30 +0530

    assignment done

--- src/routes/route.js
+++ src/routes/route.js
@@ -69,5 +69,54 @@ router.post("/test-post-4", function(req, res) {
     arr.push(ele)
     res.send(  { msg: arr , status: true }  )
 })
+ // ***
+ let players =
+ [
+     {
+         "name": "manish",
+         "dob": "1/1/1995",
+         "gender": "male",
+         "city": "jalandhar",
+         "sports": [
+             "swimming"
+         ]
+     },
+     {
+         "name": "gopal",
+         "dob": "1/09/1995",
+         "gender": "male",
+         "city": "delhi",
+         "sports": [
+             "soccer"
+         ]
+     },
+     {
+         "name": "lokesh",
+         "dob": "1/1/1990",
+         "gender": "male",
+         "city": "mumbai",
+         "sports": [
+             "soccer"
+         ]
+     },
+ ]
+ router.post('/players', function (req, res) {
+    let newPlayer = req.body
+    let newPlayersName = newPlayer.name
+    let isNameRepeated = false
+    
+    for(let i = 0; i < players.length; i++) {
+        if(players[i].name == newPlayersName) {
+            isNameRepeated = true;
+            break;
+        }
+    }
+    if (isNameRepeated) {
+        res.send("This player was already added!")
+    } else {
+        players.push(newPlayer)
+        res.send(players)
+    }
+});
 
 module.exports = router;
